76bb726fd0485261c6e9d1c368a2a4c4fac8d7ab
tonglei <tonglei@qq.com>
2018-10-26 10:22:40 +0800

    update the readme

--- README.md
+++ README.md
@@ -7,6 +7,18 @@ Look 是一款基于 CNN 训练的验证码识别工具，提供切图、训练�
 
 ## 安装
 
+### PyTorch 安装
+
+访问 https://pytorch.org/get-started/locally/
+
+找到适合自己的安装方式，如 Python3.6的安装方式：
+```
+# Python 3.6
+pip3 install http://download.pytorch.org/whl/cpu/torch-0.4.1-cp36-cp36m-win_amd64.whl
+pip3 install torchvision
+```
+
+
 ### 初次安装
 
     pip install look
@@ -28,7 +40,7 @@ python start.py
 
 # 后记
 
-本项目是基于开源项目 [pytorch-captcha-recognition](https://github.com/dee1024/pytorch-captcha-recognition)，主要做了一下功能优化：
+本项目是基于开源项目 [pytorch-captcha-recognition](https://github.com/dee1024/pytorch-captcha-recognition)，主要做了以下功能优化：
 1、train 中加入 cnn.train()
 2、recognize 中加入 cnn.eval()
 3、增加切图处理，大大减少了训练集样本数量
--- look/start.py
+++ look/start.py
@@ -6,24 +6,28 @@ from look.cut import cut_captcha, cut_train, cut_test
 from look.captcha import recognize, train, test
 
 
-# 把训练图集切图为单个字符的图集，默认已切图，重新切图需把 dataset/train 目录下图片删除
+# 把训练图集切图为单个字符的图集，本示例默认已切图，重新切图需把 **训练图集** 目录下图片删除
+# 训练图集存放路径为 setting.TRAIN_PATH 配置，默认是  dataset/train
 # cut_train()
 
-# test 切图，默认已切图，重新切图需把 dataset/test 目录下图片删除
+# test 切图，本示例默认已切图，重新切图需把 **测试图集** 目录下图片删除
+# 测试图集存放路径为 setting.TEST_PATH 配置，默认是  dataset/test
 # cut_test()
 
-# train，默认已提供训练模型，重新训练前，需要删除 model.pkl 文件
+# train，本示例默认已提供训练模型，重新训练前，需要删除 model.pkl 文件
+# model.pkl 存放目录为 setting.MODEL_PATH 配置，默认是 model 目录
 # train('model.pkl')
 
-# test，检验模型成功率
+# test，检验模型成功率，检测的是测试图集目录
 # test('model.pkl')
 
-
+# 需要失败的验证码文件路径，验证码名称可以为任意取名
 captcha_path = setting.DOWNLOAD_PATH / '5U62_1539929795.png'
 
-# 如果需要切图
+# 如果训练的是切图后的图片，那么识别也需要切图。本示例中，提供的是以切图训练后的模型
 cut_captcha(captcha_path)
 
-# 默认是识别 dataset/captcha 目录，如果为切图，会自动组合为字符串
+# 识别验证码目录的图片，如果为切图（即有多张图片），会自动组合为字符串
+# 验证码目录为 setting.CAPTCHA_PATH 配置，默认是 dataset/captcha 目录
 code = recognize('model.pkl')
 print(code)
